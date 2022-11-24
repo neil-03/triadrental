e9f02e8399e0af810883d9baca3cd0ea1646aa05
Neil <nk4733@srmist.edu.in>
2022-11-24 18:36:23 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 //load modules
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 // init app
 const app = express();
+//setup body parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+// load files
+const keys = require('./config/keys');
+//load collection
+const User = require('./models/user');
+const Contact = require('./models/contact');
+//connect to mongodb
+mongoose.connect(keys.MongoDB,{
+    useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB is connected'))
+.catch(err => console.log(err));
 // setup view engine
 app.engine('handlebars',exphbs.engine({
     deafultLayout: 'main'
@@ -21,9 +39,32 @@ app.get('/about',(req,res) => {
         title:'About'
     });
 });
+app.get('/listing',(req,res) => {
+    res.render('listing',{
+        title:'View Listings'
+    });
+});
 app.get('/contact',(req,res) => {
     res.render('contact',{
         title:'Contact us'
+    });
+});
+//SAVE contact form data
+app.post('/contact', (req,res) => {
+    console.log(req.body);
+    const newContact = {
+        
+        name:req.user._id,
+        message:req.body.message
+    }
+    new Contact(newContact).save((err,user) => {
+        if(!err) {
+            throw err;
+        }else{
+            console.log('We Recieved message form user', User);
+        }
+        
+          
     });
 });
 app.get('/signup',(req,res) => {
---
+++ config/dev.js
@@ -0,0 +1,4 @@
+//this file is used for development
+module.exports = {
+    MongoDB: 'mongodb+srv://carrental:god1234@carrental.dwvxt.mongodb.net/?retryWrites=true&w=majority'
+};
